630565ee55c220322a847b78d8c3c78a40c557a8
Jose-Dell22 <jfdell41@gmail.com>
2024-09-23 11:14:06 -0500

    Login e incio completosa
    v1 del resgistro e info

--- Inicio/Script.js
+++ Inicio/Script.js
@@ -2,18 +2,19 @@ let currentIndex = 0;
 const images = [
     '../Images/Proyecto_Robotica.jpg',
     '../Images/Proyecto_Software.jpg',
+    '../Images/Proyecto_Software_2.jpg',
+    '../Images/Proyecto_Robotica_2.jpg',
 ];
 
 function changeImage(direction) {
     const imgElement = document.getElementById('gallery-image');
-
-    imgElement.classList.add('fade-out'); 
+    imgElement.classList.add('fade-out');
 
     setTimeout(() => {
         currentIndex = (currentIndex + direction + images.length) % images.length;
         imgElement.src = images[currentIndex];
-        imgElement.classList.remove('fade-out'); 
-        updateProgressDots(); 
+        imgElement.classList.remove('fade-out');
+        updateProgressDots();
     }, 500); 
 }
 
@@ -24,7 +25,19 @@ function updateProgressDots() {
     });
 }
 
+function generateDots() {
+    const dotsContainer = document.querySelector('.progress-dots');
+    dotsContainer.innerHTML = '';
+
+    images.forEach(() => {
+        const dot = document.createElement('span');
+        dot.classList.add('dot');
+        dotsContainer.appendChild(dot);
+    });
+}
+
 document.addEventListener('DOMContentLoaded', () => {
+    generateDots();
     updateProgressDots();
-    setInterval(() => changeImage(1), 5000); 
+    setInterval(() => changeImage(1), 5000);
 });
